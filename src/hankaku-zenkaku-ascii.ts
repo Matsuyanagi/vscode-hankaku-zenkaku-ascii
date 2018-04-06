84c13148e09d7b0492d914d319ea6e2d254d0f60
Matsuyanagi <ef58ef64@gmail.com>
2018-04-07 00:35:05 +0900

    U005C_toFullWidthYenSign, fullwidthYensignTohankakuYenSign オプション実装

--- src/hankaku-zenkaku-ascii.ts
+++ src/hankaku-zenkaku-ascii.ts
@@ -15,14 +15,21 @@ export const commands_map = new Map<string, ( args: any ) => void>( [
  * @param option option parameters
  */
 function convertToHankakuAscii( option?: any ) {
+	const fullwidthYensignTohankakuYenSign = vscode.workspace.getConfiguration( 'hankakuZenkakuAscii' ).get<boolean>( 'fullwidthYensignTohankakuYenSign' )
 	convertAscii( option, ( str: string ) => {
-		return str.replace( /[　！-～“”’]/g, ( s: string ) => {
+		return str.replace( /[　！-～“”’￥]/g, ( s: string ) => {
 			if ( s === "　" ) {
 				return " "
 			} else if ( s === "“" || s === "”" ) {
 				return "\""
 			} else if ( s === "’" ) {
 				return "'"
+			} else if ( s === "￥" ) {
+				if ( fullwidthYensignTohankakuYenSign ) {
+					return "¥"
+				} else {
+					return "\\"
+				}
 			} else {
 				return String.fromCharCode( s.charCodeAt( 0 ) - 0xFEE0 )
 			}
@@ -35,9 +42,18 @@ function convertToHankakuAscii( option?: any ) {
  * @param option option parameters
  */
 function convertToZenkakuAscii( option?: any ) {
+	const U005C_toFullWidthYenSign = vscode.workspace.getConfiguration( 'hankakuZenkakuAscii' ).get<boolean>( 'U005C_toFullWidthYenSign' )
 	convertAscii( option, ( str: string ) => {
 		return str.replace( /[ -~]/g, ( s: string ) => {
-			return s === " " ? "　" : String.fromCharCode( s.charCodeAt( 0 ) + 0xFEE0 )
+			if ( s === " " ) {
+				return "　"
+			} else {
+				if ( U005C_toFullWidthYenSign && s === "\\" ) {
+					return "￥"
+				} else {
+					return String.fromCharCode( s.charCodeAt( 0 ) + 0xFEE0 )
+				}
+			}
 		} )
 	} )
 }
@@ -61,11 +77,11 @@ function convertAscii( option: any, convert: ( s: string ) => string ) {
 	editor.edit( ed => {
 		editor.selections.forEach( select => {
 			const target_range = select.isEmpty ?
-				(()=>{
+				( () => {
 					const line = editor.document.lineAt( select.start.line )
 					return new vscode.Range( line.range.start, line.range.end )
-				})()
-				 : new vscode.Range( select.start, select.end )
+				} )()
+				: new vscode.Range( select.start, select.end )
 			const text = editor.document.getText( target_range )
 			ed.replace( target_range, convert( text ) )
 		} )
